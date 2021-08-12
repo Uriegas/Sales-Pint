015ab497e2a5216fdbe7647c048f0dd054bea15b
1930526@upv.edu.mx <1930526@upv.edu.mx>
2021-08-12 16:00:25 -0500

    Test controller :chestnut:

--- src/main/java/com/uriegas/FXMLController.java
+++ src/main/java/com/uriegas/FXMLController.java
@@ -2,24 +2,40 @@ package com.uriegas;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import com.uriegas.Model.*;
+import javafx.fxml.*;
+import javafx.scene.control.*;
 
 public class FXMLController implements Initializable {
-    
-    @FXML
-    private Label label;
-    
-    @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    DataModel model;
+
+    @FXML private TextField searchbar;
+    @FXML private Button search_btn;
+    @FXML private Button clear_btn;
+    @FXML private Button settings_btn;
+    @FXML private TableView<Searchable> searchableproducts;
+    @FXML private TableColumn<Searchable, String> productname;
+    @FXML private TableColumn<Searchable, String> productdescription;
+    @FXML private TableColumn<Searchable, Double> productprice;
+    @FXML private TableView<Product> sales;
+    @FXML private TableColumn<Product, String> salesproductname;
+    @FXML private TableColumn<Product, Integer> salesproductquantity;
+    @FXML private TableColumn<Product, Double> salesproductprice;
+    @FXML private Button commitsale;
+    @FXML private Label totalprice;
+
+    public void initModel(DataModel model) {
+        if(this.model != null)
+            throw new NullPointerException("Model is already initialized, can only initialize it once");
+        else
+            this.model = model;
+        // ==> Data binding
+        // totalprice.textProperty().bindBidirectional(model.sProperty());
+        // <== Data binding
     }
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+    }
 }
--- src/main/java/com/uriegas/MainApp.java
+++ src/main/java/com/uriegas/MainApp.java
@@ -1,19 +1,24 @@
 package com.uriegas;
 
-import javafx.application.Application;
-import static javafx.application.Application.launch;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
+import javafx.application.*;
+import com.uriegas.Model.*;
+import javafx.fxml.*;
+import javafx.scene.*;
+import javafx.stage.*;
 
 
 public class MainApp extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/Scene.fxml"));
-        
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/Scene.fxml"));
+        Parent root = loader.load();
+
+        // ==> Load model
+        FXMLController controller = loader.getController();
+        controller.initModel(new DataModel());
+        // <== Load model
+
         Scene scene = new Scene(root);
         scene.getStylesheets().add("/styles/Styles.css");
         
