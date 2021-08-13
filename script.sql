fe1e33b8fc3a32f2b8b6de62f53128ca0018ed33
1930526@upv.edu.mx <1930526@upv.edu.mx>
2021-08-12 21:14:32 -0500

    Changed products table :elephant:

--- script.sql
+++ script.sql
@@ -3,7 +3,8 @@ CREATE TABLE IF NOT EXISTS products (
     id INTEGER PRIMARY KEY,
     name TEXT NOT NULL UNIQUE,
     description TEXT NOT NULL,
-    price REAL NOT NULL
+    price REAL NOT NULL,
+    stock INTEGER NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS offers (
@@ -32,6 +33,7 @@ CREATE TABLE IF NOT EXISTS orders (
     date TEXT NOT NULL
 );
 
+
 -- CREATE TRIGGER IF NOT EXISTS insert_sale AFTER DELETE ON products
 -- Theres no procedures in SQLITE >:|
 
@@ -51,3 +53,16 @@ SELECT * FROM orders WHERE id = <order_id>;
 -- Get total of ticket
 SELECT SUM(total_price) FROM sales WHERE order_id = <order_id>;
 
+--Test -- Insert rows in a Table
+
+INSERT INTO products VALUES(1001, 'Coffee', 'Coffee from Colombia', 1.99, 200);
+INSERT INTO products VALUES(1002, 'Coffee BZ', 'Coffee from Brazil', 2.99, 100);
+INSERT INTO products VALUES(1003, 'Chocolate', 'Chocolate Abuelita', 3.99, 400);
+
+INSERT INTO offers VALUES(2001, 'Como Cuates Coffe', '2x1', 1);
+
+
+-- UPSERT for a table
+INSERT INTO visits (ip, hits)
+VALUES ('127.0.0.1', 1)
+ON CONFLICT(ip) DO UPDATE SET hits = hits + 1;
