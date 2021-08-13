4ba03ef1a1299b182cf06340856e202ae9604402
1930526@upv.edu.mx <1930526@upv.edu.mx>
2021-08-12 21:10:17 -0500

    Add products or offers to the carrito :shipit:

--- src/main/java/com/uriegas/Model/DataModel.java
+++ src/main/java/com/uriegas/Model/DataModel.java
@@ -22,14 +22,33 @@ public class DataModel {
     public static String OFFERS = "offers";
     public static String SALES = "sales";
     public static String ORDERS = "orders";//An order is a bunch of sales
-    public static int ID = 1, NAME = 2, DESCRIPTION = 3, TYPE = 3, PRICE = 4, PRODUCT_ID = 4;
+    public static int ID = 1, NAME = 2, DESCRIPTION = 3, TYPE = 3, PRICE = 4, PRODUCT_ID = 4, STOCK = 5;
 
     private Connection connection;
-    StringProperty s = new SimpleStringProperty();
+    //The cart is a list of products
+    private ObservableList<Product> cart = FXCollections.observableArrayList();
+    DoubleProperty totalpriceProperty = new SimpleDoubleProperty(0.0);
+    //  cart.forEach(p -> p.getPrice()).sum(p -> p.getPrice());
+
+    //Total price property to be equal to the sum of the prices times the quantity of the products in the cart
+    public DoubleProperty totalpriceProperty() {
+        return totalpriceProperty;
+    }
+    public void setTotalPrice(double totalprice) {
+        this.totalpriceProperty.set(totalprice);
+    }
+    public double getTotalPrice() {
+        return totalpriceProperty.get();
+    }
 
     public DataModel() {
-        setS("Hola");
         setConnection();
+        cart.addListener(new ListChangeListener<Product>() {
+            @Override
+            public void onChanged(Change<? extends Product> c) {
+                setTotalPrice(c.getList().stream().mapToDouble(p -> p.getPrice() * p.getStock()).sum());
+            }
+        });
     }
 
     public void setConnection() {
@@ -40,30 +59,68 @@ public class DataModel {
             connection = null;
         }
     }   
-
-    public StringProperty sProperty() { return s; }
-    public String getS() { return s.get(); }
-    public void setS(String s) { this.s.set(s); }
+    public ObservableList<Product> cartProperty() {
+        return cart;
+    }
+    public void addToCart(Searchable searchable) {
+        if(searchable instanceof Product) {
+            Product product = (Product) searchable;
+            for(Product p : cart) {
+                if(p.getId() == product.getId()) {
+                    p.setStock(p.getStock() + 1);
+                    return;
+                }
+            }
+            cart.add(new Product(product.getId(), product.getName(), product.getDescription(), product.getPrice(), 1));
+            // cart.add(product);
+        } else if(searchable instanceof Offer) {
+            Offer offer = (Offer) searchable;
+            System.out.println("Implement parsing offers: " + offer.getId());
+        }
+    }
+    public void addToCart(Product product) {
+        cart.add(product);
+    }
+    public void removeFromCart(Product product) {
+        cart.remove(product);
+    }
+    public void clearCart() {
+        cart.clear();
+    }
+    public void insertCartToDB() throws SQLException {
+        for(Product product : cart)
+            upsertProduct(product);
+        System.out.println(INFO + " " + LocalDate.now() + " " + cart.size() + " products added to the cart");
+    }
+    /**
+     * Insert or update a product in the database 
+     */
+    public void upsertProduct(Product product) throws SQLException {
+        String query = "INSERT INTO " + PRODUCTS + " VALUES (?, ?, ?, ?, ?, ?)" +
+                       " ON CONFLICT (id) DO UPDATE SET name = " + product.getName() +
+                       ", description = " + product.getDescription() +
+                       ", price = " + product.getPrice() + ", stock = " + product.getStock();
+        Statement statement = connection.createStatement();
+        statement.executeUpdate(query);
+        System.out.println(INFO + " " + LocalDate.now() + " " + "Product " + product.getName() + " inserted");
+    }
 
     /**
      * Get products and offers from database
      */
     public ObservableList<Searchable> getSearchables() {
         // Write query for a join btw products and offers
-        String query = "SELECT products.id, products.name, products.price, offers.quantity, offers.price, offers.id "
-                + "FROM products "
-                + "LEFT JOIN offers ON products.id = offers.product_id "
-                + "ORDER BY products.name";
+        String query = "SELECT * FROM " + PRODUCTS;
         // Create a list to store the products
         ObservableList<Searchable> searchables = FXCollections.observableArrayList();
         try {
-            // Open connection
             Statement stmt = connection.createStatement();
             ResultSet rs = stmt.executeQuery(query);
-            // Get products from database
-            while (rs.next()) { // Instantiate products and offers
-            }
-            // Close connection
+            while (rs.next())
+                searchables.add(new Product(rs.getInt(ID), rs.getString(NAME), rs.getString(DESCRIPTION), rs.getDouble(PRICE), rs.getInt(STOCK) ));
+            rs = stmt.executeQuery("SELECT * FROM " + OFFERS);
+            while (rs.next())
+                searchables.add(new Offer(rs.getInt(ID), rs.getString(NAME), rs.getString(TYPE), rs.getInt(PRODUCT_ID)));
             stmt.close();
             rs.close();
         } catch (SQLException e) {
@@ -79,7 +136,7 @@ public class DataModel {
         Statement stmt = connection.createStatement();
         ResultSet rs = stmt.executeQuery(query);
         while (rs.next())
-            products.add(new Product(rs.getInt(ID), rs.getString(NAME), rs.getString(DESCRIPTION), rs.getInt(PRICE)));
+            products.add(new Product(rs.getInt(ID), rs.getString(NAME), rs.getString(DESCRIPTION), rs.getInt(PRICE), rs.getInt(STOCK)));
         stmt.close();
         rs.close();
         return products;
